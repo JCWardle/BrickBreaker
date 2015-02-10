ad09f56effbe8cf91892f9c0d4aab708b2e926c2
Jordan Wardle <jordan@ninjatertle.com>
2015-02-10 16:07:13 +1100

    Implmeneted brick creation and rendering.

--- BrickBreaker/app.js
+++ BrickBreaker/app.js
@@ -1,27 +1,54 @@
-﻿var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+﻿var Brick = (function () {
+    function Brick() {
+        this.colour = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")";
+        this.width = 50;
+        this.height = 10;
+        this.alive = true;
     }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () {
-            return _this.span.innerHTML = new Date().toUTCString();
-        }, 500);
+    return Brick;
+})();
+
+var BrickBreaker = (function () {
+    function BrickBreaker(canvas) {
+        this._canvas = canvas;
+        this._bricks = new Array(5);
+
+        for (var i = 0; i < this._bricks.length; i++) {
+            this._bricks[i] = new Array(15);
+            for (var j = 0; j < this._bricks[i].length; j++) {
+                this._bricks[i][j] = new Brick();
+            }
+        }
+    }
+    BrickBreaker.prototype.update = function () {
     };
 
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
+    BrickBreaker.prototype.render = function () {
+        var context = this._canvas.getContext("2d");
+
+        for (var y = 0; y < this._bricks.length; y++) {
+            for (var x = 0; x < this._bricks[y].length; x++) {
+                var brick = this._bricks[y][x];
+
+                if (brick.alive) {
+                    context.fillStyle = brick.colour;
+                    context.fillRect(x * brick.width, y * brick.height, brick.width, brick.height);
+                }
+            }
+        }
+        //Render Player
     };
-    return Greeter;
+
+    BrickBreaker.prototype.play = function () {
+        this.update();
+        this.render();
+    };
+    return BrickBreaker;
 })();
 
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var game = new BrickBreaker(canvas);
+    game.play();
 };
 //# sourceMappingURL=app.js.map
--- BrickBreaker/app.ts
+++ BrickBreaker/app.ts
@@ -1,28 +1,65 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿class Brick {
+    colour: string;
+    width: number;
+    height: number;
+    alive: boolean;
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+    constructor() {
+        this.colour = "rgb(" + Math.floor(Math.random() * 255)
+            + "," + Math.floor(Math.random() * 255) + ","
+            + Math.floor(Math.random() * 255) + ")";
+        this.width = 50;
+        this.height = 10;
+        this.alive = true;
+    }
+}
+
+class BrickBreaker {
+    private _canvas : HTMLCanvasElement;
+    private _bricks : Array<Array<Brick>>;
+
+    constructor(canvas : HTMLCanvasElement) {
+        this._canvas = canvas;
+        this._bricks = new Array<Array<Brick>>(5);
+
+        for (var i = 0; i < this._bricks.length; i++) {
+            this._bricks[i] = new Array<Brick>(15);
+            for (var j = 0; j < this._bricks[i].length; j ++) {
+                this._bricks[i][j] = new Brick();
+            }
+        }
     }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    private update() {
+
     }
 
-    stop() {
-        clearTimeout(this.timerToken);
+    private render() {
+        var context = this._canvas.getContext("2d");
+        
+        //Render Bricks        
+        for (var y = 0; y < this._bricks.length; y++) {
+            for (var x = 0; x < this._bricks[y].length; x++) {
+                var brick = this._bricks[y][x];
+
+                if (brick.alive) {
+                    context.fillStyle = brick.colour;
+                    context.fillRect(x * brick.width, y * brick.height, brick.width, brick.height);
+                }
+            }
+        }
+
+        //Render Player
     }
 
+    play() {
+        this.update();
+        this.render();
+    }
 }
 
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var game = new BrickBreaker(canvas);
+    game.play();
 };
